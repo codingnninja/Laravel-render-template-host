f2f6b76e8a668dd2d68476a9813f1abd174ac7c9
codingnninja <ayobami_ogundiran@yahoo.com>
2023-06-11 18:15:57 +0100

    Update 00-laravel-deploy.sh
    Remove cloudinary provider setup

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,5 +11,3 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force 
 
-echo "Publishing cloudinary provider..."
-php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tag="cloudinary-laravel-config"
